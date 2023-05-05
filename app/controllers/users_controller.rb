bdfba57cb202cd859e62331514e9701c4cc082d7
Vagner Begni <vagner.begni@gmail.com>
2023-05-05 08:09:35 -0300

    Tela administração usuários ok - faltam testes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:admin, :financial, :employee, :resident]
+  
   def index
+    @users = User.all
+  end
+
+  def admin
+    @user.admin!
+    redirect_to users_path
+  end
+
+  def financial
+    @user.financial!
+    redirect_to users_path
+  end
+
+  def employee
+    @user.employee!
+    redirect_to users_path
+  end
+
+  def resident
+    @user.resident!
+    redirect_to users_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # Defines the root path route ("/")
   root to: 'home#index'
-  get '/users', to: 'users#index', as: 'users'
+  #get '/users', to: 'users#index', as: 'users'
+
+  resources :users, only: [:index] do
+    post 'admin', on: :member
+    post 'financial', on: :member
+    post 'employee', on: :member
+    post 'resident', on: :member
+  end
 end
