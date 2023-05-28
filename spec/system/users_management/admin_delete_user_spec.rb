64f0f65844d93c905513558666132a73fac27676
Vagner Begni <vagner.begni@gmail.com>
2023-05-27 21:51:48 -0300

    Feature ok: Single Admin shouldnt exclude itself

--- spec/system/users_management/admin_delete_user_spec.rb
+++ spec/system/users_management/admin_delete_user_spec.rb
@@ -2,24 +2,25 @@ require 'rails_helper'
 
 describe 'Admin deletes a user...' do
   it 'successfully if there are others admins' do
-    admin = User.create!(email: 'morador001@email.com', password: 'morador001', role: 'admin')
-    user = User.create!(email: 'morador002@email.com', password: 'morador002', role: 'admin')
+    admin_a = User.create!(email: 'morador001@email.com', password: 'morador001', role: 'admin')
+    admin_b = User.create!(email: 'morador002@email.com', password: 'morador002', role: 'admin')
 
-    login_as admin
+    login_as admin_a
     visit root_path
     within('nav') do
       click_on 'Administrador'
     end
-    within("tr#user_#{user.id}") do
+    within("tr#user_#{admin_b.id}") do
       click_on 'Excluir'
     end
 
     expect(page).to have_content('Usuário excluído com sucesso.')
-    expect(page).not_to have_content(user.email)
+    expect(page).not_to have_content(admin_b.email)
   end
 
-  it 'and get error if its the only Admin' do
+  it 'and go to signin page if it deletes itself' do
     admin_a = User.create!(email: 'morador001@email.com', password: 'morador001', role: 'admin')
+    User.create!(email: 'morador002@email.com', password: 'morador002', role: 'admin')
 
     login_as admin_a
     visit root_path
@@ -30,13 +31,22 @@ describe 'Admin deletes a user...' do
       click_on 'Excluir'
     end
 
-    expect(page).to have_content('Falha na Exclusão! Sistema não pode ficar sem administrador.')
-    within("tr#user_#{admin_a.id}") do
+    expect(current_path).to eq user_session_path
+  end
+
+  it 'and dont see delete button if its the single Admin' do
+    admin = User.create!(email: 'morador001@email.com', password: 'morador001', role: 'admin')
+
+    login_as admin
+    visit root_path
+    within('nav') do
+      click_on 'Administrador'
+    end
+
+    within("tr#user_#{admin.id}") do
       expect(page).to have_content('Administrador')
       expect(page).not_to have_button('Administrador')
-      expect(page).to have_button('Financeiro')
-      expect(page).to have_button('Funcionário')
-      expect(page).to have_button('Morador')
+      expect(page).not_to have_button('Excluir')
     end
   end
 end
